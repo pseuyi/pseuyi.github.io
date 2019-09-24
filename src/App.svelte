738ae2fe02418bfdb02d094cf481ebce65fdfa09
pseuyi <pseuyi@gmail.com>
2019-09-23 21:45:55 -0400

    init

--- src/App.svelte
+++ src/App.svelte
@@ -8,4 +8,5 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>(^^)/</h1>
+<iframe width="50%" height="100%" src="https://docs.google.com/document/d/e/2PACX-1vRN_qkq2YrILmLNBLwiBOgVUN7WtCeCCTn3I7X-MiOolGhxSxlxDUn1FNrBoYZXfJD6SKMFIXI_2h3V/pub?embedded=true"></iframe>
