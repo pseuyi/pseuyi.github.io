6bdf95d13391e24d77435634df9b60ca461fe8ad
pseuyi <pseuyi@gmail.com>
2020-02-18 23:12:53 -0500

    move projects to cmpt

---
+++ src/Projects.svelte
@@ -0,0 +1,28 @@
+<ol>
+  <li><h2>web</h2></li>
+  <ul>
+    <li><a href="/modes">webmidi piano w modes</a></li>
+    <li><a href="/2048">elm + 2048</a></li>
+    <li><a href="/sequencer">sequencer</a></li>
+    <li>
+      <a href="https://iso-note.herokuapp.com/">markdown + html converter</a>
+    </li>
+    <li><a href="/magiclamp">web terminal</a></li>
+    <li>
+      <a href="https://vespertine-rhythms.herokuapp.com/">connect places</a>
+    </li>
+  </ul>
+
+  <li><h2>sketchbook</h2></li>
+  <ul>
+    <li><a href="/grammar">rita + processing</a></li>
+    <li><a href="http://tilde.learning-gardens.co/~freda/">tilde club</a></li>
+  </ul>
+
+  <li><h2>archive</h2></li>
+  <ul>
+    <li><a href="/2017">2017</a></li>
+    <li><a href="/2016">2016</a></li>
+  </ul>
+</ol>
+
