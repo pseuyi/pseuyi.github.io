b47f5637d7e38ec35b5788a499206ace931a0d0c
pseuyi <pseuyi@gmail.com>
2020-02-20 10:40:45 -0500

    comment out some links

--- src/Projects.svelte
+++ src/Projects.svelte
@@ -13,15 +13,7 @@
     </li>
   </ul>
 
-  <li><h2>sketches</h2></li>
-  <ul>
-    <li><a href="/grammar">rita + processing</a></li>
-    <li><a href="http://tilde.learning-gardens.co/~freda/">tilde club</a></li>
-
-    <li><a href="/e-doser">mushrooms in vr</a></li>
-  </ul>
-
-  
+  <!-- 
   <li><h2>writing</h2></li>
   <header><a href="https://pseuyi.github.io/blog">about programming</a></header>
   <br/>
@@ -30,12 +22,32 @@
     <li><a href="http://topicalcream.info/editorial/simulating-feeling-xavier-cha/">simulating feedback</a></li>
     <li><a href="http://topicalcream.info/editorial/martine-syms-veritcal-elevated-oblique/">vertical elevated oblique</a></li>
   </ul>
+ -->
 
-  <li><h2>speaking</h2></li>
+  <li><h2>workshops</h2></li>
   <ul>
-    <li><a href="https://github.com/pseuyi/react-ladies-lightning">writing react strategically @ react ladies</a></li>
-    <li><a href="https://github.com/pseuyi/gdi-core-git-github">intro to git and github @ girl develop it hacktober</a></li>
-    <li><a href="https://gist.github.com/pseuyi/407944acd0b4f09a8f9686d32f844117">intro to javascript @ girl develop it</a></li>
+    <li>
+      <a href="https://github.com/pseuyi/react-ladies-lightning"
+        >writing react strategically @ react ladies</a
+      >
+    </li>
+    <li>
+      <a href="https://github.com/pseuyi/gdi-core-git-github"
+        >intro to git and github @ girl develop it hacktober</a
+      >
+    </li>
+    <li>
+      <a href="https://gist.github.com/pseuyi/407944acd0b4f09a8f9686d32f844117"
+        >intro to javascript @ girl develop it</a
+      >
+    </li>
+  </ul>
+  <li><h2>sketches</h2></li>
+  <ul>
+    <li><a href="/grammar">rita + processing</a></li>
+    <li><a href="http://tilde.learning-gardens.co/~freda/">tilde club</a></li>
+
+    <li><a href="/e-doser">mushrooms in vr</a></li>
   </ul>
 
   <li><h2>archive</h2></li>
